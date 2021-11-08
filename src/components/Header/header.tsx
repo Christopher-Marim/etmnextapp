aa9ddab168146fd65593e337cddcc117022aff22
Christopher Marim <cssmarim1@gmail.com>
2021-11-08 20:12:46 -0300

    menu to phone

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,60 +1,62 @@
-import { useEffect, useState } from "react";
-import { resultLanguages } from "../../pages";
-import styles from "./Header.module.scss";
+import { useEffect, useState } from 'react'
+import { resultLanguages } from '../../pages'
+import styles from './Header.module.scss'
 import { useRouter } from 'next/router'
+import { BiMenu } from 'react-icons/bi'
 
 interface HeaderProps {
-  options: Option[];
-  logo: string;
-  languages: resultLanguages[];
+  options: Option[]
+  logo: string
+  languages: resultLanguages[]
 }
 
 export interface Option {
-  id: number;
-  name: string;
+  id: number
+  name: string
 }
 
-const I18N_STORAGE_KEY = "i18nextLng";
+const I18N_STORAGE_KEY = 'i18nextLng'
 
 export function Header({ options, logo, languages }: HeaderProps) {
-  const [language, setLanguage] = useState("");
+  const [language, setLanguage] = useState('')
+  const [statusMenu, setStatusMenu] = useState(false)
   const [style, setStyle] = useState({
-    background: "rgba(0, 0, 0, 0.0)",
-  });
+    background: 'rgba(0, 0, 0, 0.0)'
+  })
 
   const router = useRouter()
 
   useEffect(() => {
     setLanguage(
-      (typeof window !== "undefined" &&
+      (typeof window !== 'undefined' &&
         localStorage.getItem('language_id')) as string
-    );
-  }, []);
+    )
+  }, [])
 
-  if (typeof window !== "undefined") {
-    window.onscroll = () => navbarScroll();
+  if (typeof window !== 'undefined') {
+    window.onscroll = () => navbarScroll()
   }
   const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    if (typeof window !== "undefined") {
-      localStorage.setItem("language_id", event.target.value);
-      if(event.target.value=='1'){
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('language_id', event.target.value)
+      if (event.target.value == '1') {
         router.push('/')
       }
-      if(event.target.value=='2'){
+      if (event.target.value == '2') {
         router.push('/en')
       }
-      if(event.target.value=='3'){
+      if (event.target.value == '3') {
         router.push('/es')
       }
     }
-  };
+  }
 
   function navbarScroll() {
-    var y = window.scrollY;
+    var y = window.scrollY
     if (y > 30) {
-      setStyle({ background: "rgba(0, 0, 0, 0.50)" });
+      setStyle({ background: 'rgba(0, 0, 0, 0.50)' })
     } else if (y < 30) {
-      setStyle({ background: "rgba(0, 0, 0, 0.0)" });
+      setStyle({ background: 'rgba(0, 0, 0, 0.0)' })
     }
   }
 
@@ -62,23 +64,58 @@ export function Header({ options, logo, languages }: HeaderProps) {
     <div id={styles.layout1Container} style={{ ...style }}>
       <img src={logo} alt="logo" />
       <div className={styles.wrapper}>
-        {options.map((option) => (
-          <a
-            href={`#${option.name}`}
-            style={{ borderBottomColor: "#169179" }}
-            key={option.id}
-          >
-            {option.name.toUpperCase()}
-          </a>
-        ))}
-        <select onChange={handleSelectChange} value={language}>
-          {languages.map((lng) => (
-            <option key={lng.id} value={`${lng.language_id}`}>
-              {lng.abreviation}
-            </option>
+        <div className={styles.buttons}>
+          {options.map(option => (
+            <a
+              href={`#${option.name}`}
+              style={{ borderBottomColor: '#169179' }}
+              key={option.id}
+            >
+              {option.name.toUpperCase()}
+            </a>
           ))}
-        </select>
+          <select onChange={handleSelectChange} value={language}>
+            {languages.map(lng => (
+              <option key={lng.id} value={`${lng.language_id}`}>
+                {lng.abreviation}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div
+          className={styles.buttonMenu}
+          onClick={() => setStatusMenu(!statusMenu)}
+        >
+          <BiMenu size={45}></BiMenu>
+        </div>
+        {statusMenu && (
+          <div className={styles.menu}>
+            <div
+              className={styles.buttonExit}
+              onClick={() => setStatusMenu(!statusMenu)}
+            >
+              X
+            </div>
+            {options.map(options => (
+              <a
+                href={`#${options.name}`}
+                onClick={() => setStatusMenu(false)}
+                style={{ borderBottomColor: '#169179' }}
+                key={options.id}
+              >
+                {options.name.toUpperCase()}
+              </a>
+            ))}
+             <select onChange={handleSelectChange} value={language}>
+            {languages.map(lng => (
+              <option key={lng.id} value={`${lng.language_id}`}>
+                {lng.abreviation}
+              </option>
+            ))}
+          </select>
+          </div>
+        )}
       </div>
     </div>
-  );
+  )
 }
